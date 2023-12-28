ca323dd080b1177dd3d71fdd3c99dee452068fe2
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-12-28 17:25:35 +0900

    프로젝트 업데이트

--- src/main/java/com/dita/holoprism/security/config/SpringSecurityConfig.java
+++ src/main/java/com/dita/holoprism/security/config/SpringSecurityConfig.java
@@ -4,15 +4,17 @@ import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().permitAll());
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(request -> request
+                    .dispatcherTypeMatchers(DispatcherType.REQUEST).permitAll()
+                    .anyRequest().permitAll());
         return http.build();
     }
 }
--- src/main/java/com/dita/holoprism/user/controller/UserController.java
+++ src/main/java/com/dita/holoprism/user/controller/UserController.java
@@ -4,18 +4,16 @@ import com.dita.holoprism.user.dto.RegisterDto;
 import com.dita.holoprism.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
 
+    @ResponseBody
     @PostMapping("/create")
     public ResponseEntity<?> createUser(@Validated @RequestBody RegisterDto dto) {
         return ResponseEntity.ok(userService.createUser(dto));

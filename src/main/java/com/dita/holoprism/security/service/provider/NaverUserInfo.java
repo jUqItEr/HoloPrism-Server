8d38b9b3ebaf995bc24a9ed5746308dc06c87ece
myeongseung <myeongseung8952@gmail.com>
2024-01-02 15:10:59 +0900

    네이버, 카카오 회원가입 처리완료

---
+++ src/main/java/com/dita/holoprism/security/service/provider/NaverUserInfo.java
@@ -0,0 +1,37 @@
+package com.dita.holoprism.security.service.provider;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+
+    private Map<String, Object> attributes;
+
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getSocialId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public String getSocialType() {
+        return "naver";
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getImage() {
+        return null;
+    }
+}
--- src/main/java/com/dita/holoprism/user/repository/UserRepository.java
+++ src/main/java/com/dita/holoprism/user/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.dita.holoprism.user.repository;
 
 import com.dita.holoprism.user.dto.ProviderDto;
 import com.dita.holoprism.user.entity.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +14,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByIdAndProvider(String id, String provider);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE user_tbl SET visited_at = now() WHERE id = :userId", nativeQuery = true)
+    void updateVisitedTime(@Param("userId") String userId);
 }

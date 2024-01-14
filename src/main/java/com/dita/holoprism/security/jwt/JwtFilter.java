7660892a566618e42b78aea55337ac7586394cfe
DelynMk2 <dnjs1174@gmail.com>
2024-01-15 00:58:54 +0900

    - JWT 로그아웃, 재발급 기능 개선

--- src/main/java/com/dita/holoprism/security/jwt/JwtFilter.java
+++ src/main/java/com/dita/holoprism/security/jwt/JwtFilter.java
@@ -1,14 +1,21 @@
 package com.dita.holoprism.security.jwt;
 
+import com.dita.holoprism.security.auth.PrincipalDetails;
+import com.dita.holoprism.user.entity.UserEntity;
+import com.dita.holoprism.user.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
@@ -16,28 +23,46 @@ import org.springframework.web.filter.GenericFilterBean;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
 
    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
    public static final String AUTHORIZATION_HEADER = "Authorization";
-   private JwtTokenProvider jwtTokenProvider;
-   public JwtFilter(JwtTokenProvider jwtTokenProvider) {
-      this.jwtTokenProvider = jwtTokenProvider;
-   }
+   private final JwtTokenProvider jwtTokenProvider;
+   private final UserRepository userRepository;
 
    @Override
    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
       HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+      HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
       String jwt = resolveToken(httpServletRequest);
       String requestURI = httpServletRequest.getRequestURI();
 
-      if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+      if (StringUtils.hasText(jwt) && (jwtTokenProvider.validateToken(jwt).equals("ACCESS"))) {
          Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
          SecurityContextHolder.getContext().setAuthentication(authentication);
          logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-      } else {
+      } else if (StringUtils.hasText(jwt) && (jwtTokenProvider.validateToken(jwt).equals("EXPIRED"))){
+         logger.debug("Access token expired, uri: {}", requestURI);
+         String refreshToken = null;
+         PrincipalDetails principalDetails = (PrincipalDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+         UserEntity user = principalDetails.getUser();
+         user.setPassword(null);
+         if (StringUtils.hasText(user.getId())) { // 로그인 정보를 통해 ID값을 다시 받아옴
+            refreshToken = userRepository.getUserRefreshToken(user.getId()); // userId로 refreshToken 조회
+         }
 
-         logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+         // DB에 저장된 refresh token 검증
+         if (StringUtils.hasText(refreshToken) && jwtTokenProvider.validateToken(refreshToken).equals("ACCESS")) {
+            // access token 재발급
+            Authentication authentication = jwtTokenProvider.getAuthentication(refreshToken);
+            String newAccessToken = jwtTokenProvider.createToken(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            httpServletResponse.setHeader(AUTHORIZATION_HEADER, newAccessToken);
+            logger.info("Reissue access token");
+         }
+         // refresh token이 없거나 잘못된 경우 바로 logout
       }
 
       filterChain.doFilter(servletRequest, servletResponse);
